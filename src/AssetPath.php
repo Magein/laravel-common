95848ea16d9ad65803b64c7aa7fd9278e307fc87
小马哥 <1154699897@qq.com>
2022-01-21 16:28:40 +0800

    update

--- src/AssetPath.php
+++ src/AssetPath.php
@@ -5,6 +5,23 @@ namespace Magein\Common;
 class AssetPath
 {
     /**
+     * 去掉带domain的地址
+     * @param $path
+     * @return string
+     */
+    public static function toSavePath($path): string
+    {
+        if ($path) {
+            if (is_array($path)) {
+                $path = current($path);
+            }
+            $path = trim(parse_url($path)['path'] ?? '', '/');
+        }
+
+        return $path;
+    }
+
+    /**
      * 将路径中的public替换成static
      * @param $save_path
      * @return string
@@ -18,6 +35,11 @@ class AssetPath
         return preg_replace('/^public/', 'static', $save_path);
     }
 
+    /**
+     * 获取预览的地址，包含路径的
+     * @param $save_path
+     * @return string
+     */
     public static function getVisitPath($save_path): string
     {
         if (empty($save_path)) {
